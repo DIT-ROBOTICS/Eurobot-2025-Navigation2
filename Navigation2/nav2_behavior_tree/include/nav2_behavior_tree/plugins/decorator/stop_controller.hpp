84f4d367b18c545ba9c60acf7d3db27a76b077e3
112062109 <justinshih@gapp.nthu.edu.tw>
2025-03-01 16:07:33 +0800

    Add StopController decorator node to manage robot stopping functionality

---
+++ Navigation2/nav2_behavior_tree/include/nav2_behavior_tree/plugins/decorator/stop_controller.hpp
@@ -0,0 +1,27 @@
+#ifndef NAV2_BEHAVIOR_TREE__PLUGINS__DECORATOR__STOP_CONTROLLER_HPP_
+#define NAV2_BEHAVIOR_TREE__PLUGINS__DECORATOR__STOP_CONTROLLER_HPP_
+#include "behaviortree_cpp_v3/decorator_node.h"
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+namespace nav2_behavior_tree
+{
+    class StopController : public BT::DecoratorNode
+    {
+        public:
+            StopConrtoller(
+                const std::string & name,
+                const BT::NodeConfiguration & conf);
+            static BT::PortsList providedPorts();
+            BT::NodeStatus tick() override;
+        private:
+            void stopCallbacke(const std_msgs::msg::Bool::SharedPtr msg);
+            rclcpp::Node::SharedPtr node_;
+            rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr stop_sub;
+            rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub;
+            bool stop_robot;
+    }
+}
+
+#endif  // NAV2_BEHAVIOR_TREE__PLUGINS__DECORATOR__STOP_CONTROLLER_HPP_
---
+++ Navigation2/nav2_behavior_tree/plugins/decorator/stop_controller.cpp
@@ -0,0 +1,47 @@
+#include "nav2_behavior_tree/plugins/decorator/stop_controller.hpp"
+
+namespace nav2_behavior_tree
+{
+    StopController::StopController(
+        const std::string & name,
+        const BT::NodeConfiguration & conf)
+    : BT::DecoratorNode(name, conf),
+      stop_robot(false)
+    {
+        node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
+        cmd_vel_pub = node_->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+        stop_sub = node_->create_subscription<std_msgs::msg::Bool>("stop", 10, std::bind(&StopController::stopCallback, this, std::placeholders::_1));
+    }
+
+    BT::NodeStatus StopController::providedPorts()
+    {
+        return {
+            BT::InputPort<bool>("stop")
+        };
+    }
+
+    BT::NodeStatus StopController::tick()
+    {
+        if (stop_robot)
+        {
+            geometry_msgs::msg::Twist cmd_vel;
+            cmd_vel.linear.x = 0.0;
+            cmd_vel.angular.z = 0.0;
+            cmd_vel_pub->publish(cmd_vel);
+            stop_robot = false;
+            return BT::NodeStatus::FAILURE;
+        }
+        return child_node_->executeTick();
+    }
+
+    void StopController::stopCallback(const std_msgs::msg::Bool::SharedPtr msg)
+    {
+        stop_robot = msg->data;
+    }
+} // namespace nav2_behavior_tree
+
+#include "behaviortree_cpp_v3/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+    factory.registerNodeType<nav2_behavior_tree::StopController>("StopController");
+}
