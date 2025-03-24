bc94adaf6c29fdc6d6c0237423c555f2f0eab100
112062109 <justinshih@gapp.nthu.edu.tw>
2025-03-25 01:51:26 +0800

    Add success flag to docking server and handle stop condition to avoid collisions

--- opennav_docking/opennav_docking/src/docking_server.cpp
+++ opennav_docking/opennav_docking/src/docking_server.cpp
@@ -418,6 +418,11 @@ bool DockingServer::approachDock(Dock * dock, geometry_msgs::msg::PoseStamped & 
     if (!controller_->computeVelocityCommand(target_pose.pose, command, dock_backwards_)) {
       throw opennav_docking_core::FailedToControl("Failed to get control");
     }
+    controller_->computeIfNeedStop(target_pose.pose, command);
+    if(command.linear.x == 0 && command.linear.y == 0 && command.angular.z == 0) {
+      RCLCPP_INFO(get_logger(), "Stopping robot to avoid collision with rival");
+      success_ = false;
+    }
     vel_publisher_->publish(command);
 
     if (this->now() - start > timeout) {
