c64d8c62c8a5fe26ccfd0f864b00d090e7d75179
Rabbit025879 <rabbit025879@gmail.com>
2025-05-19 06:36:11 +0800

    Modify: enhance logging output with color formatting in execute method

--- Navigation2/nav2_behaviors/include/nav2_behaviors/timed_behavior.hpp
+++ Navigation2/nav2_behaviors/include/nav2_behaviors/timed_behavior.hpp
@@ -184,7 +184,7 @@ protected:
   // onRun and cycle functions to execute a specific behavior
   void execute()
   {
-    RCLCPP_INFO(logger_, "Running %s", behavior_name_.c_str());
+    RCLCPP_INFO(logger_, "\033[1;35m Running %s \033[0m", behavior_name_.c_str());
 
     if (!enabled_) {
       RCLCPP_WARN(
